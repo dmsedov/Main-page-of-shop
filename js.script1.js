1cccc5469d38996241e133426a97e551160519e5
dmsedov <s.d.92@yandex.ru>
2017-04-13 15:07:20 +0300

    Add files via upload
    dowload of files

---
+++ js.script1.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+$(function(){
+$('.popular-img').height($('.popular-img').width()/1.253);
+	$(window).resize(function(){
+    $('.popular-img').height($('.popular-img').width()/1.253);
+  });
+});
+
+//  $('.categories-item').height($('.categories-item').width()/1.2048);
+//	  $(window).resize(function(){
+//    $('.categories-item').height($('.categories-item').width()/2.5);
+//  });
