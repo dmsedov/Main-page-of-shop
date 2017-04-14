872622d2cb1e8b7b40c09bedc95b940bd2db4611
dmsedov <s.d.92@yandex.ru>
2017-04-14 23:21:21 +0300

    New html-file Index
    Which update contain responsive slider which one was created by using CCS3 specification and addings media queries.

--- js.script1.js
+++ js.script1.js
@@ -5,8 +5,9 @@ $('.popular-img').height($('.popular-img').width()/1.253);
     $('.popular-img').height($('.popular-img').width()/1.253);
   });
 });
-
-//  $('.categories-item').height($('.categories-item').width()/1.2048);
-//	  $(window).resize(function(){
-//    $('.categories-item').height($('.categories-item').width()/2.5);
-//  });
+$(window.onscroll=function() {
+    var HeaderBg = document.querySelector('.header-page-top');
+    if (HeaderBg) {
+        HeaderBg.classList.add('scroll-background');
+    }
+});
